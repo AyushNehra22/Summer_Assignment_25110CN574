6f433033fab66bc8410b0fe01bc1f0f5a3fcdeb5
AyushNehra22 <ayushnehra2611@gmail.com>
2026-06-07 07:09:00 +0530

    Day-7 assignment is uploaded

---
+++ Day-7/q1.cpp
@@ -0,0 +1,14 @@
+//wap to recursive facctorial
+#include<iostream>
+using namespace std;
+int factorial(int n){
+    if(n==0){
+        return 1;
+    }
+     return n*factorial(n-1);
+    }
+ int main(){
+    int n;
+    cin>>n;
+    cout<<"factorial is "<<factorial(n)<<endl;
+ }
---
+++ Day-7/q3.cpp
@@ -0,0 +1,14 @@
+//wap to recursive sum og digits
+#include<iostream>
+using namespace std;
+int sumOfDigits(int n){
+    if(n==0){
+        return 0;
+    }
+    return (n%10) + sumOfDigits(n/10);
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<"Sum of digits is "<<sumOfDigits(n)<<endl;
+}

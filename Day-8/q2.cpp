cebb853b9c9267e2137f23f4a7229c03a7c318e3
AyushNehra22 <ayushnehra2611@gmail.com>
2026-06-08 07:17:55 +0530

    Day-8 assignment us uploaded

---
+++ Day-8/q2.cpp
@@ -0,0 +1,18 @@
+//wap to print number triangle
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 5;
+  
+    int i=1;
+    while(i<=5){
+        int j=1;
+        while(j<=i){
+            cout<<j<<" ";
+            j= j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+    return 0;
+}
---
+++ Day-8/q3.cpp
@@ -0,0 +1,23 @@
+//wap to print character triangle
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+        int i=1;
+        while(i<=n){
+        int j=1;
+        char ch = 'A';
+        while(j<=i){
+            
+            cout<<ch<<" ";
+            j=j+1;
+            ch = ch+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+    return 0;
+}

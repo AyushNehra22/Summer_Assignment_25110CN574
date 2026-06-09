b15ec6b975cbbb7215bd98c54fd3cf232548a7e7
AyushNehra22 <ayushnehra2611@gmail.com>
2026-06-09 08:33:59 +0530

    Day-9 assignment is uploaded

---
+++ Day-9/q4.cpp
@@ -0,0 +1,23 @@
+//Wap to print hollow square pattern
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            if(i==1 || i==n || j==1 || j==n){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+    return 0;
+}
